b8ca6370c5165ba82d478e04e918c1e59abe5fa1
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-03-14 19:00:59 -0500

    se creo proyecto

---
+++ src/main/java/backend/mcsvventas/McsvVentasApplication.java
@@ -0,0 +1,13 @@
+package backend.mcsvventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvVentasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsvVentasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/mcsvventas/McsvVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.mcsvventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsvVentasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
