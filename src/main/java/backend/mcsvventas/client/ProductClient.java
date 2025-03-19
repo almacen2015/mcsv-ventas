217240865ddda41d87d96124efb10af28f5ee467
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-03-19 18:49:23 -0500

    se modifico url de feign de producto

--- src/main/java/backend/mcsvventas/client/ProductClient.java
+++ src/main/java/backend/mcsvventas/client/ProductClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "mcsv-productos", url = "http://localhost:8081/api/producto")
+@FeignClient(name = "mcsv-productos", url = "http://localhost:9000/api/producto")
 public interface ProductClient {
     @GetMapping("/{id}")
     ProductoDtoResponse getProduct(@PathVariable Integer id);
